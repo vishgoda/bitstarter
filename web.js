a83daf04a4246e160d59485788419e69fe8266dd
Vish Goda <vishgoda@gmail.com>
2013-07-11 23:30:47 +0000

    updated to read from file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
 
+var file_buf = fs.readFileSync('./index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+ 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(file_buf);
 });
 
 var port = process.env.PORT || 5000;
