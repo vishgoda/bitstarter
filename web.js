8b6e4fda831ff5b7fb31fb8c9a60af37cc4525a2
Vish Goda <vishgoda@gmail.com>
2013-07-20 20:21:16 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
+var fs = require('fs'); 
 
-var file_buf = fs.readFileSync('./index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
+var file_buf = fs.readFileSync('./index.html', "utf-8"); 
+  if (!file_buf)
+     throw new Error("cannot open index.htm file");
 
  
 var app = express.createServer(express.logger());
@@ -13,7 +12,7 @@ app.get('/', function(request, response) {
   response.send(file_buf);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
